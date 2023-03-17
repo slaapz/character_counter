b779572809e899257fc896fd7a4b7ee4c6fa82b1
Patrick Ball <slaapz@gmail.com>
2023-03-16 20:15:51 -0600

    added counting functionality with js

---
+++ counter.js
@@ -0,0 +1,16 @@
+const textAreaEl = document.getElementById('textarea');
+const totalCounterEl = document.getElementById('total-counter');
+const remainingCounterEl = document.getElementById('remaining-counter');
+
+textAreaEl.addEventListener('keyup', () => {
+    updateCounter()
+})
+
+function updateCounter() {
+   totalCounterEl.innerText =  textAreaEl.value.length;
+   remainingCounterEl.innerText = textAreaEl.getAttribute('maxlength') - textAreaEl.value.length;
+
+   
+}
+
+updateCounter();
